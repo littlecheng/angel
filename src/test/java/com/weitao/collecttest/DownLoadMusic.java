320ca030ef61baf01ad4fa2535922059cb472553
littlecheng <599013081@qq.com>
2020-03-25 13:31:24 +0800

    修改歌曲下载

--- src/test/java/com/weitao/collecttest/DownLoadMusic.java
+++ src/test/java/com/weitao/collecttest/DownLoadMusic.java
@@ -7,7 +7,7 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.*;
 
-public class HttpRequestUtil
+public class DownLoadMusic
 {
     static String contextPath = "E://music//";
 
